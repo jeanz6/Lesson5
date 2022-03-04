058238ab0533fb1c6fb33c6e7988fe267c95ac3a
DVDope <66468274+DVDope@users.noreply.github.com>
2022-03-04 11:41:11 +0100

    Create docs_test.java
    add java file with some javadoc string

---
+++ src/backend/docs_test.java
@@ -0,0 +1,22 @@
+public class Main {
+    int x = 5;
+
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+  }
+  
+  
+  /**
+  * Returns the sum of all squared int values. Takes three int values as input.
+  *
+  * @param x  first int value to be given to method
+  * @param y  seconde int value to be given to method
+  * @param z  third int value to be given to method
+  *
+  * @return   integer value
+  */
+  public static int testing(int x, int y, int z) {
+      return res = x ** 2 + y ** 2 + z ** 2;
+  }
+}
